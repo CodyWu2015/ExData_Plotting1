ec4477b7eef953bca67892ed02f63b6d3d21f016
CodyWu2015 <wuchenmax@hotmail.com>
2015-06-07 14:12:45 -0400

    add code to download and extract data so it is fully reproducible

--- plot1.R
+++ plot1.R
@@ -1,4 +1,19 @@
-a <- read.csv("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", colClasses=c(rep("character", 2), rep("numeric", 7)))
+if(!file.exists("data"))
+{
+  dir.create("data")
+}
+if(!file.exists("./data/household_power_consumption.zip"))
+{
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl, destfile="./data/household_power_consumption.zip", method="curl")
+}
+
+if(!file.exists("./data/household_power_consumption.txt"))
+{
+  unzip("data/household_power_consumption.zip", exdir="data")
+}
+
+a <- read.csv("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", colClasses=c(rep("character", 2), rep("numeric", 7)))
 a$Date <- as.Date(a$Date, format="%d/%m/%Y")
 a <- subset(a, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 png(file="plot1.png", width=480, height=480)
